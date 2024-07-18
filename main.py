8464256bb42818c80916786101a147b0ea29bf7f
siddhantchaudhary036 <160422817+siddhantchaudhary036@users.noreply.github.com>
2024-07-18 23:28:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,13 @@ def autologin():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-software-rasterizer")
 
     service = Service(executable_path = driver_path)
-    browser = webdriver.Chrome(service = service)
+    browser = webdriver.Chrome(service = service, options=chrome_options)
 
     browser.get(url)
     time.sleep(1)
